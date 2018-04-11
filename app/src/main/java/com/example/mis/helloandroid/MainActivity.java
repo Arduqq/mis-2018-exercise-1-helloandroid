13e36a41940f0351c7516f2cfbe83bcf8a7cb1d7
Arduqq <twop@hotmail.de>
2018-04-11 16:55:09 +0200

    First implementation, untested

--- app/src/main/java/com/example/mis/helloandroid/MainActivity.java
+++ app/src/main/java/com/example/mis/helloandroid/MainActivity.java
@@ -1,10 +1,23 @@
 package com.example.mis.helloandroid;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,5 +32,34 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(MainActivity.this, "hey there", Toast.LENGTH_SHORT);
         toast.setGravity(Gravity.CENTER, 0 ,0);
         toast.show();
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void connectToURL(View view) throws IOException {
+        EditText editText = (EditText)findViewById(R.id.textInputLayout);
+        TextView textView = (TextView)findViewById(R.id.textView);
+        String searchURL = (String) editText.getText().toString();
+
+        try {
+            String html = "";
+            URL url = new URL(searchURL);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder str = new StringBuilder();
+            String line = null;
+            while((line = reader.readLine()) != null) {
+                str.append(line);
+            }
+            in.close();
+            html = str.toString();
+            textView.setText(html);
+        } catch(Exception e) {
+            Toast toast = Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0 ,0);
+            toast.show();
+        }
+
     }
 }
